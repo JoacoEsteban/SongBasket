cce400788310afccff5c89ee3bd9783274a4fefe
Joaco Esteban <dev.joaco.esteban@gmail.com>
2019-10-19 14:11:36 -0300

    logs removed

--- src/renderer/store/modules/CurrentUser.js
+++ src/renderer/store/modules/CurrentUser.js
@@ -107,6 +107,7 @@ const actions = {
     })
   },
   unsyncPlaylist ({commit}, id) {
+    console.log('dodifajdfjklasdjf')
     return new Promise((resolve, reject) => {
       commit('UNSYNC_PLAYLIST', id)
       resolve()
