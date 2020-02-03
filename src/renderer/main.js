31972cf830681dec607056ba49a00a3632d551ac
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-02-02 23:35:09 -0300

    WIP custom-made title bar

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,34 +4,61 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
-const customTitlebar = require('custom-electron-titlebar')
+const { BrowserWindow } = require('electron').remote
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+(function () {
+  function init () {
+    document.getElementById('min-btn').addEventListener('click', function (e) {
+      var window = BrowserWindow.getFocusedWindow()
+      window.minimize()
+    })
+    console.log(document.getElementById('max-btn'))
+    document.getElementById('max-btn').addEventListener('click', function (e) {
+      var window = BrowserWindow.getFocusedWindow()
+      window.maximize()
+    })
+
+    document.getElementById('close-btn').addEventListener('click', function (e) {
+      var window = BrowserWindow.getFocusedWindow()
+      window.close()
+    })
+  }
+
+  document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+      init()
+    }
+  }
+})()
 
 new Vue({
-  components: { App },
+  components: {
+    App
+  },
   router,
   store,
   template: '<App/>'
 }).$mount('#app')
 
 /* eslint-disable no-unused-vars */
-console.log(customTitlebar.Themebar.mac)
-let titlebar
-let isMac = process.platform === 'darwin'
+// console.log(customTitlebar.Themebar.mac)
+// let titlebar
+// let isMac = process.platform === 'darwin'
 
-function createTitleBar () {
-  titlebar = new customTitlebar.Titlebar({
-    backgroundColor: customTitlebar.Color.fromHex('#333'),
-    iconsTheme: customTitlebar.Themebar[isMac ? 'mac' : 'win'],
-    titleHorizontalAlignment: 'right',
-    order: isMac ? 'inverted' : null,
-    unfocusEffect: false
-  })
+// function createTitleBar () {
+//   titlebar = new customTitlebar.Titlebar({
+//     backgroundColor: customTitlebar.Color.fromHex('#333'),
+//     iconsTheme: customTitlebar.Themebar[isMac ? 'mac' : 'win'],
+//     titleHorizontalAlignment: 'right',
+//     order: isMac ? 'inverted' : null,
+//     unfocusEffect: false
+//   })
 
-  document.documentElement.style.setProperty('--max-container-height', 'calc(100vh - ' + document.querySelector('.titlebar').offsetHeight + 'px)')
-}
+//   document.documentElement.style.setProperty('--max-container-height', 'calc(100vh - ' + document.querySelector('.titlebar').offsetHeight + 'px)')
+// }
 
-createTitleBar()
+// createTitleBar()
