c9289efc0fb41f5e0b07f7e3e84373231fa232c6
Joaco Esteban <dev.joaco.esteban@gmail.com>
2021-12-31 14:23:07 -0300

    ADD, node global types

--- src/@types/node.d.ts
+++ src/@types/node.d.ts
@@ -1,12 +1,19 @@
+import { SongBasketId } from './SongBasket'
+import { SpotifyUser } from './Spotify'
+
 declare module NodeJS {
   // import sudo from 'sudo-prompt'
   interface Global {
     pathExists: typeof import('path-exists')
     sudo: typeof import('sudo-prompt').exec
     ipcSend: typeof import('../main/controllers/InitializationAndHandlers/ipc.controller').default.send
+    USER_ID: SpotifyUser['id'],
+    SONGBASKET_ID: SongBasketId,
     // ------------------------------------------
     ENV_PROD: boolean
     IS_DEV: boolean
+    CONNECTED_TO_INTERNET: boolean
+    CONNECTED_TO_API: boolean
     CONSTANTS: import('./constants').Constants,
     log: (...args: any) => void,
     emptyFn: () => void,
@@ -17,6 +24,7 @@ declare module NodeJS {
     SESSION_FOLDER_PATHS: {
       paths: string[],
       selected: string | null
-    }
+    },
+    openUrl: typeof import('open'),
   }
 }
