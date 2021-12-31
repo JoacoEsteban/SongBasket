1b5aacca0776179ef6d26ece792b3d4a470c2d6d
Joaco Esteban <dev.joaco.esteban@gmail.com>
2021-12-31 14:22:54 -0300

    ADD & FIX, SongBasket types

--- src/@types/SongBasket.ts
+++ src/@types/SongBasket.ts
@@ -1,29 +1,32 @@
+import { TrackList } from '../main/controllers/FileSystem/FileWatchers'
+import { Constants } from './constants'
 import { SpotifyPlaylist, SpotifyPlaylistId, SpotifyTrack, SpotifyTrackId, SpotifyUser } from './Spotify'
 import { TrackConversionResponse, YouTubeResultId } from './YouTube'
 
-export type SongbasketTrack = {
+export type SongBasketTrackQuery = {
+  query: string,
+  duration: 'long' | 'medium' | 'short',
+  duration_s: number,
+  id: SpotifyTrackId
+}
+
+
+export type SongBasketTrack = {
   id: string,
-  data: {
-    album: SpotifyTrack,
-    playlists: ({
-      id: SpotifyPlaylistId
-    })[],
-    conversion: TrackConversionResponse,
-    selection: null | false | YouTubeResultId,
-    custom: null | YouTubeResultId,
-    query: {
-      query: string,
-      duration: string,
-      duration_s: number,
-      id: SpotifyTrackId
-    },
-    flags: {
-      converted: boolean,
-      conversionError: boolean,
-      processed: boolean,
-    },
-    duration: number,
-  }
+  data: SpotifyTrack,
+  playlists: ({
+    id: SpotifyPlaylistId
+  })[],
+  conversion: TrackConversionResponse | null,
+  selection: null | false | YouTubeResultId,
+  custom: null | YouTubeResultId,
+  query?: SongBasketTrackQuery | null,
+  flags: {
+    converted: boolean,
+    conversionError: boolean,
+    processed: boolean,
+  },
+  duration: number | null,
 }
 
 export type SongBasketSaveFile = {
@@ -33,7 +36,7 @@ export type SongBasketSaveFile = {
   queuedPlaylists: SpotifyPlaylistId[],
   cachedPlaylists: SpotifyPlaylistId[],
   deletedPlaylists: SpotifyPlaylistId[],
-  convertedTracks: SongbasketTrack[],
+  convertedTracks: SongBasketTrack[],
   currentPlaylist: string,
   control: {
     total: number,
@@ -54,3 +57,32 @@ export enum SongbasketCustomMp3TagNormalized {
   spotify_id = 'spotify_id',
   youtube_id = 'youtube_id',
 }
+
+export type LoadingController = {
+  target: string[]
+  count: number
+  set: (target: string, value: number) => void
+  reflect: (payload: any) => void
+  get instance (): { value: boolean, target: string }
+  get isLoading (): boolean
+  get which (): string
+  get canRequest (): boolean
+  // -----------------------
+  get on (): (target: string) => void
+  get off (): (target: string) => void
+  ptg (target: string, ptg: number): void
+  fn (val: number): (target: string) => void
+}
+
+
+export type AppStatus = {
+  APP_STATUS: Constants["APP_STATUS"],
+  state: SongBasketSaveFile | null,
+  downloadedTracks: TrackList | null,
+  FFMPEG_BINS_DOWNLOADED: boolean,
+  CONNECTED_TO_INTERNET: boolean,
+  CONNECTED_TO_API: boolean,
+  error: Error | null
+}
+
+export type SongBasketId = string
