2301689f94bdc35576cefd787bb1c4b52bd1d6b0
Joaco Esteban <dev.joaco.esteban@gmail.com>
2022-01-01 21:56:32 -0300

    FIX, global type definitions

--- src/@types/node.d.ts
+++ src/@types/node.d.ts
@@ -1,27 +1,29 @@
 import { SongbasketFoldersFile, SongBasketId } from './SongBasket'
 import { SpotifyUser } from './Spotify'
 
-declare module NodeJS {
-  // import sudo from 'sudo-prompt'
-  interface Global {
-    pathExists: typeof import('path-exists')
-    sudo: typeof import('sudo-prompt').exec
-    ipcSend: typeof import('../main/controllers/InitializationAndHandlers/ipc.controller').default.send
-    USER_ID: SpotifyUser['id'],
-    SONGBASKET_ID: SongBasketId,
-    // ------------------------------------------
-    ENV_PROD: boolean
-    IS_DEV: boolean
-    CONNECTED_TO_INTERNET: boolean
-    CONNECTED_TO_API: boolean
-    CONSTANTS: import('./constants').Constants,
-    log: (...args: any) => void,
-    emptyFn: () => void,
-    __static: string,
-    flushYtDlCache: () => Promise<string>,
-    FEATURES: {},
-    HOME_FOLDER: string,
-    SESSION_FOLDER_PATHS: SongbasketFoldersFile,
-    openUrl: typeof import('open'),
+declare global {
+  namespace NodeJS {
+    // import sudo from 'sudo-prompt'
+    interface Global {
+      pathExists: typeof import('path-exists')
+      sudo: typeof import('sudo-prompt').exec
+      ipcSend: typeof import('../main/controllers/InitializationAndHandlers/ipc.controller').default.send
+      USER_ID: SpotifyUser['id'],
+      SONGBASKET_ID: SongBasketId,
+      // ------------------------------------------
+      ENV_PROD: boolean
+      IS_DEV: boolean
+      CONNECTED_TO_INTERNET: boolean
+      CONNECTED_TO_API: boolean
+      CONSTANTS: import('./constants').Constants,
+      log: (...args: any) => void,
+      emptyFn: () => void,
+      __static: string,
+      flushYtDlCache: () => Promise<string>,
+      FEATURES: {},
+      HOME_FOLDER: string,
+      SESSION_FOLDER_PATHS: SongbasketFoldersFile,
+      openUrl: typeof import('open'),
+    }
   }
 }
